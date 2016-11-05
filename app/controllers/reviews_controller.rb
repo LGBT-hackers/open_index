ff7215fbbe8635623418ad3eca1a32e7f3d58408
eripheebs <erikapheby@gmail.com>
2016-11-05 23:41:07 +0000

    fixed typo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user! :except => [:index, :show]
+  before_action :authenticate_user!, :except => [:index, :show]
 
   def index
   end
