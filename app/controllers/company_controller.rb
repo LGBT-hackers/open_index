cede366d60c6c8a0cd00319af40a7d30b07e3476
Jazzy <jasmina.gasper@gmail.com>
2016-11-05 14:48:08 +0000

    httparty

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -5,10 +5,10 @@ class CompanyController < ApplicationController
     if search_contains_characters(params)
       @companies =  Company.where('lower(name) LIKE ?', "%#{@searched_term}%".downcase).all.order(:name)
       if @companies.count == 0
-        "http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}"
+        @companies = HTTParty.get("http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}")
       end
     else
-      @companies = Company.all.order(:name
+      @companies = Company.all.order(:name)
     end
     @company_names = get_unique(@companies)
   end
