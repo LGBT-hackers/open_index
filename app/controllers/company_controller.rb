97ae3d6d6fa3491e703d8cf69757615a9768d17e
Jamie Brown <jamiebrown201@hotmail.com>
2016-11-05 14:05:43 +0000

    wip

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -4,8 +4,11 @@ class CompanyController < ApplicationController
     @searched_term = params[:name]
     if search_contains_characters(params)
       @companies =  Company.where('lower(name) LIKE ?', "%#{@searched_term}%".downcase).all.order(:name)
+      if @companies.count == 0
+        "http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}"
+      end
     else
-      @companies = Company.all.order(:name)
+      @companies = Company.all.order(:name
     end
     @company_names = get_unique(@companies)
   end
