5076f51f047df9ae6397d4962ae47e690f9ccefc
ZeshanRasul <zeshanrasul90@gmail.com>
2016-11-05 22:03:30 +0000

    Shows individual reviews and ratings on company page

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ApplicationRecord
 
   belongs_to :user
   belongs_to :company
+
+  def average_review_rating
+    (question_1 + question_2 + question_3 + question_4 + question_5 + question_6)/6
+  end
 end
