2244cb2573ad413f761af505c8d4790b498281fc
Edward Withers <edward.withers@surfdome.com>
2016-11-05 15:41:13 +0000

    add associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+
+  has_many :ratings
+  has_many :reviews
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,5 @@
 class Rating < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :company
 end
