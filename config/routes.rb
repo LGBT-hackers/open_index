9b668363b7ff2d0eb068a73480b5ad8594f69361
Radu Asandei <rasandei@deloitte.co.uk>
2016-11-05 14:49:35 +0000

    Fixed confirmation email functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	root :to => 'home#index'
-	devise_for :users, controllers: { confirmations: 'confirmations' }
+	devise_for :users
 
 end
