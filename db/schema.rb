01a70aa218e7ceb4d286b45827dd2715b932d119
Edward Withers <edward.withers@surfdome.com>
2016-11-06 11:36:59 +0000

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20161106085020) do
     t.text     "description"
     t.integer  "company_id"
     t.string   "category"
+    t.integer  "user_id"
     t.integer  "question_1"
     t.integer  "question_2"
     t.integer  "question_3"
@@ -45,27 +46,26 @@ ActiveRecord::Schema.define(version: 20161106085020) do
     t.integer  "question_6"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id"
     t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
@@ -73,5 +73,4 @@ ActiveRecord::Schema.define(version: 20161106085020) do
 
   add_foreign_key "flags", "reviews"
   add_foreign_key "flags", "users"
-  add_foreign_key "reviews", "users"
 end
