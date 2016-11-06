9034b6346ce8f07e4c7ca299a854d32ab401ac43
Edward Withers <edward.withers@surfdome.com>
2016-11-06 07:54:10 +0000

    tidy up migrations

--- db/migrate/20161105232324_remove.rb
+++
@@ -1,5 +0,0 @@
-class Remove < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :reviews, :user_id
-  end
-end
--- db/migrate/20161105232409_add_user_id_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToReviews < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :reviews, :user, index: true, foreign_key: true
-  end
-end
