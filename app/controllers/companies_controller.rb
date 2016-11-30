0bb5a8743d35026f048d08b3a142f4dcfbd036d7
Jamie Brown <jamiebrown201@hotmail.com>
2016-11-30 17:13:20 +0000

    wip

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,16 +1,14 @@
 class CompaniesController < ApplicationController
   include CompaniesHelper
   before_filter :nav_search_render, only: [:index]
-  
+
   def index
     @company = Company.new
+    @api_companies = HTTParty.get("http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}")["response"]["employers"]
     @searched_term = params[:name]
     if search_contains_characters(params)
       @companies =  Company.where('lower(name) LIKE ?', "%#{@searched_term}%".downcase).all.order(:name)
       @api_companies = []
-      if @companies.count == 0
-         @api_companies = HTTParty.get("http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}")["response"]["employers"]
-      end
     else
       @companies = Company.all
     end
