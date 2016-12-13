1484a687320b4a402725765c9824184aeec228b6
Jamie Brown <jamiebrown201@hotmail.com>
2016-12-13 16:11:04 +0000

    refactor

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,12 +6,10 @@ class CompaniesController < ApplicationController
   def index
     @company = Company.new
     @searched_term = params[:name]
+    @api_companies = []
+    @api_companies = HTTParty.get("http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}")["response"]["employers"]
     if search_contains_characters(params)
       @companies =  Company.where('lower(name) LIKE ?', "%#{@searched_term}%".downcase).all.order(:name)
-      @api_companies = []
-      if @companies.count == 0
-         @api_companies = HTTParty.get("http://api.glassdoor.com/api/api.htm?t.p=#{ENV["GD_ID"]}&t.k=#{ENV["GD_KEY"]}&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{@searched_term}")["response"]["employers"]
-      end
     else
       @companies = Company.all
     end
@@ -22,9 +20,7 @@ class CompaniesController < ApplicationController
   end
 
   def new
-    if params["api"]
-      company_create
-    end
+    company_create if params["api"]
     @company = Company.new
   end
 
